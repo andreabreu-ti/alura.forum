d5931b9c9db1cf78f5150bde33f74f9de56e5f37
andre_abreu <andre_abreu@04B-IN001606.agropalma.net>
2025-11-10 13:13:29 -0300

    Detalhando tópicos

--- alura.forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ alura.forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -6,12 +6,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.alura.forum.controller.dto.DetalhesTopicoDto;
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.entity.Topico;
@@ -30,20 +32,35 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 
 	/**
+	 * Método para DETALHAR topicos por ID
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@GetMapping("/{id}")
+	public DetalhesTopicoDto detalhar(@PathVariable Long id) {
+
+		Topico topico = topicoRepository.getOne(id);
+		return new DetalhesTopicoDto(topico);
+
+	}
+
+	/**
 	 * Método para CADASTRAR tópicos
 	 * 
 	 * @param form
-	 * @Valid Identifica que quando os dados vierem da requisição, o spring rode as validações criada no TopicoForm
+	 * @Valid Identifica que quando os dados vierem da requisição, o spring rode as
+	 *        validações criada no TopicoForm
 	 */
 	@PostMapping
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 
 		Topico topico = form.coverter(cursoRepository);
 		topicoRepository.save(topico);
-		
+
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
-		
-		return ResponseEntity.created(uri).body(new TopicoDto(topico)); //retorna 201
+
+		return ResponseEntity.created(uri).body(new TopicoDto(topico)); // retorna 201
 	}
 
 	/**
